13d04cff69711f846e4fe1e18a70ad237454d38d
mohamadi msa <mohamadi.msa-abdou@epitech.eu>
2020-12-07 00:06:38 +0100

    emplementation des option et du man

--- function.php
+++ function.php
@@ -1,35 +1,85 @@
 <?php 
+// recursive revoir function recur
 
+// fonction permettant de recupérer le nom de l'image passer en argv[2]
+function recupe_name(string $option){
+  $cute = strstr($option, "=");
+  $name = substr($cute,1);
+  return $name;
+}
+// fonction regex 
+
+function regex(string $reg, string $str){
+$pattern = "/$reg/i";
+$result = preg_match($pattern, $str);
+if($result == 1){
+	return true;
+}
+else{return false ;}
+}
+
+//fonction qui renvois un message d'erreur si le nombre d'image png et inferieure a 1 ou inexistant 
+
+function error_image(){
+    global $search_pictures;
+	if(empty($search_pictures)){
+		echo "Error : aucune image de format png n'a était trouver dans ce dossier ".PHP_EOL;
+		return 0;}
+	if(count($search_pictures) <2){
+		echo "Error : le nombre d'image present dans le fichier n'est pas suffisant pour excuter le proprame veillez a avoir au minimum 2 image png".PHP_EOL;
+	   return 0;
+	}
+return 1;
+}
+// function permettant de charger et redimentioner les images
+
+ function charge_image(){
+
+	  global $search_pictures;
+
+	for($i = 0; $i < count($search_pictures);$i++){
+	  $image[$i] = imagecreatefrompng($search_pictures[$i]);
+		resize($search_pictures[$i], $i);
+  }   
+
+	  $image_resize = glob("*resize.png");
+
+	  for($i = 0; $i < count($image_resize);$i++){
+		$image_r[$i] = imagecreatefrompng($image_resize[$i]);
+
+  } 
+      return $image_r;  
+  }
 
 // function permettant de generer du css
-function generate_css(){  
+function generate_css(string $name = "style"){  
 	
-	   if(file_exists("style.css")){unlink ("style.css");}
+	$name_css = $name.".css";
+	   if(file_exists("$name_css")){unlink("$name_css");}
 	   
 	   $images = glob("*.png");
 	   $name_ima ="";
-			$name_image = fopen("style.css", "a+");
+			$name_image = fopen("$name_css", "a+");
 			for($i = 0 ; $i < count($images);$i++){
 			   $name_ima .= ".".$images[$i].", ";
 		 }
 		  
-		 $name_ima .= "\n{ display: inline-block; background: url('sprite.png') no-repeat; overflow: hidden; text-indent: -9999px; text-align: left; }\n";
-		  
+		 $name_ima .= "\n{\n display: inline-block;\n background: url('sprite.png') no-repeat;\n overflow: hidden;\n text-indent: -9999px;\n text-align: left;\n }\n\n";
 		 
-	
+	      
 		  for($i = 0 ; $i < count($images);$i++){
-			 $name_ima .= ".".$images[$i]."{ background-position: -0px -0px; width: 1600px; height: 900px; }\n";
+			  $positon = $i*200;
+			 $name_ima .= ".".$images[$i]."{\n background-position: -0px $positon"."px;\n width: 200px;\n height: 200px;\n }\n\n";
 	
 		  }
 		  fwrite($name_image ,$name_ima);
 	
 		  fclose($name_image);
-	
 }
 
 // Fonction permettant de concaténer tous images se trouvant dans le dossier courant
 
-function merge_image($resource, $name_picture = "sprite"){
+function merge_image(array $resource, string $name_picture = "sprite"){
 
 
 	 $taille= 200 *count($resource);
@@ -76,7 +126,41 @@ $height = 200;
  imagepng($image_p, $num_image."resize.png");
 
  }
-
+// function debug var_dump en simplifier 
  function d($variable){
 	 var_dump($variable);
+ }
+
+ // fonction man css_generator
+
+ function man(){
+
+		echo "Utilisation : css_generator [OPTION]...assets_folder ".PHP_EOL;
+		echo PHP_EOL;
+        echo"Concaténez toutes les images d'un dossier dans un sprite et écris une feuille de style prête à l'emploi.\nLes arguments obligatoires pour les options longues sont également obligatoires pour les options courtes".PHP_EOL;
+		echo "\n   -r, --recursive      Recherchez des images dans le dossier assets_folder passé comme argument et dans tous ses sous-répertoires.\n".PHP_EOL;
+		echo "   -i, --output-image=IMAGE     Nom de l'image générée. S'il est vide, le nom par défaut est «sprite.png».\n".PHP_EOL;
+		echo "   -s, --output-style=STYLE     Nom de la feuille de style générée. S'il est vide, le nom par défaut est «style.css».".PHP_EOL;
+	}
+
+ // fonction 
+ 
+ function recur(){
+	function list_dir($name, $level=0) {
+		if ($dir = opendir($name)) {
+		  while($file = readdir($dir)) {
+			for($i=1; $i<=(4*$level); $i++) {
+				echo " ";
+				
+			}
+			echo "$file".PHP_EOL;
+			if(is_dir($file) && !in_array($file, array(".",".."))) {
+			  list_dir($file,$level+1);
+			}
+		  }
+		  closedir($dir);
+		}
+	  }
+	  list_dir(".");
+
  }
