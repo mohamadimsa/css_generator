91b58190e4e9fad165811f985a6a71fcbe97696f
mohamadi msa <mohamadi.msa-abdou@epitech.eu>
2020-12-11 19:56:22 +0100

    dossier

--- function.php
+++ function.php
@@ -72,10 +72,7 @@ function error_image(){
 }
 // function permettant de charger et redimentioner les images
 
- function charge_image(){
-
-	  $image_charge = glob("*.png");
-
+ function charge_image($image_charge){
 
 	for($i = 0; $i < count($image_charge);$i++){
 	  $image[$i] = imagecreatefrompng($image_charge[$i]);
@@ -91,12 +88,14 @@ function error_image(){
   }
 
 // function permettant de generer du css
-function generate_css(string $name = "style"){  
-	
+function generate_css(array $images,string $name = "style"){  
+	  
+		
 	$name_css = $name.".css";
 	   if(file_exists("$name_css")){unlink("$name_css");}
+
 	   
-	   $images = glob("*.png");
+	  
 	   $name_ima ="";
 			$name_image = fopen("$name_css", "a+");
 			for($i = 0 ; $i < count($images);$i++){
@@ -185,5 +184,94 @@ $height = 200;
  // fonction 
  
  function recur(){
-	
+	$dossier = [];
+$i = -1;
+if ($handle = opendir('.')) {
+  while (false !== ($entry = readdir($handle))) {
+    
+      if ($entry != "." && $entry != "..") {
+          if(is_dir($entry) && is_readable($entry) &&  $entry != ".git"){
+            $i ++;
+            $dossier[$i] = $entry;
+          }
+      }
+  }
+  closedir($handle);
+}
+
+$im_t = [];
+for($i = 0 ; $i < count($dossier);$i++)
+{
+  if ($handle = opendir($dossier[$i])) {
+    $entry = readdir($handle);
+     $tr_dos = getcwd();
+     $tr_dos .= "/".$dossier[$i];
+    
+     chdir($tr_dos);
+     $im = glob("*png");
+     if(count($im)> 0){
+        
+          foreach($im as $value){
+
+            array_push($im_t,getcwd()."/".$value);
+          }
+           
+      
+     }
+     
+    $dos_courant = dirname(getcwd());
+    chdir($dos_courant);
+     
+    }
+}
+for($i = 0; count($im_t) > $i; $i++){
+
+$imagename = substr(strrchr($im_t[$i], "/"), 1);
+$name_do = strrev($im_t[$i]);
+$name_do = strstr($name_do, '/');
+$name_do = substr($name_do,1);
+$name_do = strrev($name_do);
+$name_do = substr(strrchr($name_do, "/"), 1);
+$path_image[$i] = $name_do."/".$imagename;
+
+}
+
+$image_dc = glob("*png");
+for($i = 0;count($image_dc) > $i ; $i++)
+{
+  $imageamerge[$i] = $image_dc[$i];
+}
+if(count($path_image) > 0){
+   foreach($path_image as $value)
+   {
+     array_push($imageamerge,$value);
+   }
+
+}
+
+merge_image(charge_image($imageamerge));
+
+  
+foreach($imageamerge as $key => $value){
+  
+  $imagename = substr(strrchr($value, "/"), 1);
+    array_push($imageamerge,$imagename);
+  
+  if(str_contains($value, "/"))
+  {
+    
+
+    unset($imageamerge[$key]);
+    
+  }
+  if(strlen($value)== 0){
+    unset($imageamerge[$key]);
+  }
+}
+  
+ $imageamerge= array_filter($imageamerge);
+ 
+ d($imageamerge);
+ $imageamerge= array_merge($imageamerge);
+ generate_css($imageamerge);
  }
