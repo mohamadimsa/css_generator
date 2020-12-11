497f043c9ec82fcbb9700dd1ee66e0aa462bfdff
mohamadi msa <mohamadi.msa-abdou@epitech.eu>
2020-12-11 19:19:03 +0100

    -r ok

--- test.php
+++ test.php
@@ -43,9 +43,9 @@ for($i = 0 ; $i < count($dossier);$i++)
 }
 for($i = 0; count($im_t) > $i; $i++){
 
-  $imagename = substr(strrchr($im_t[$i], "/"), 1);
- $name_do = strrev($im_t[$i]);
- $name_do = strstr($name_do, '/');
+$imagename = substr(strrchr($im_t[$i], "/"), 1);
+$name_do = strrev($im_t[$i]);
+$name_do = strstr($name_do, '/');
 $name_do = substr($name_do,1);
 $name_do = strrev($name_do);
 $name_do = substr(strrchr($name_do, "/"), 1);
@@ -53,4 +53,41 @@ $path_image[$i] = $name_do."/".$imagename;
 
 }
 
-d($path_image);
+$image_dc = glob("*png");
+for($i = 0;count($image_dc) > $i ; $i++)
+{
+  $imageamerge[$i] = $image_dc[$i];
+}
+if(count($path_image) > 0){
+   foreach($path_image as $value)
+   {
+     array_push($imageamerge,$value);
+   }
+
+}
+
+//merge_image(charge_image($imageamerge));
+
+  
+foreach($imageamerge as $key => $value){
+  
+  $imagename = substr(strrchr($value, "/"), 1);
+    array_push($imageamerge,$imagename);
+  
+  if(str_contains($value, "/"))
+  {
+    
+
+    unset($imageamerge[$key]);
+    
+  }
+  if(strlen($value)== 0){
+    unset($imageamerge[$key]);
+  }
+}
+  
+ $imageamerge= array_filter($imageamerge);
+ 
+ d($imageamerge);
+ $imageamerge= array_merge($imageamerge);
+ generate_css($imageamerge);
